0969966f633325b372fd6f4e7d677a0bd93b5d87
AbhishekSonkarr <abhisheksonkrr@gmail.com>
2025-12-07 17:31:33 +0530

    GFG

---
+++ day_7/minimumPlatroms.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int minPlatform(vector<int>& arr, vector<int>& dep) 
+    {
+        sort(arr.begin(),arr.end()); 
+        sort(dep.begin(),dep.end());
+        
+        int i=0,j=0;
+        int n=arr.size();
+        int maxx=0, count =0; 
+        while (i<n && j<n)
+        {
+            if (arr[i]<=dep[j]) 
+            {
+                count ++; //to count no. of platform require
+                maxx = max(maxx,count); 
+                i++;
+            }else 
+            {
+                j++;
+                count --; // when there is departure
+            }
+            
+            
+        }return maxx;    
+    }
+};
