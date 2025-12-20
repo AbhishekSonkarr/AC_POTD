4d058dc404d637d354996c40c287789e49eee4bc
AbhishekSonkarr <abhisheksonkrr@gmail.com>
2025-12-20 14:01:19 +0530

    leetcode(medium) - 3163

---
+++ day_19/string_compression.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp;
+
+        int n = word.size(), i = 0;
+        while (i < n) {
+            char c = word[i];
+            int count = 0;
+            while (c == word[i] && count < 9) {
+                count++;
+                i++;
+            }
+            comp += to_string(count);
+            comp.push_back(c);
+        }
+        return comp;
+    }
+};//time complexity O(N)
